8a1ae227814d982591a0c0f9ecc7ad474481e711
Dragonborn98760 <mateusz98760@gmail.com>
2019-10-21 10:19:24 +0200

    Base

---
+++ FightSim/FightSim/Fighter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Fighter
+    {
+
+        private int hp;
+
+        public string name;
+
+        
+
+        public void Attack(int damage)
+        {
+
+        }
+
+        public void Hurt(int amount)
+        {
+
+        }
+
+        public void IsAlive (bool IsAlive)
+        {
+
+        }
+
+        public void GetHP (int hp)
+        {
+
+        }
+
+    }
+}
