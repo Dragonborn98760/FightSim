42c7c75ad179339106f3c070020836bdee2e1bfc
Dragonborn98760 <43043232+Dragonborn98760@users.noreply.github.com>
2019-11-22 21:16:38 +0100

    Friday p5

--- FightSim/FightSim/Program.cs
+++ FightSim/FightSim/Program.cs
@@ -21,14 +21,12 @@ namespace FightSim
             //imput check for valid number
             while (!int.TryParse(Console.ReadLine(), out heroNumber))
             {
-                Console.WriteLine(heroNumber);
-                Console.ReadLine();
+                
                 //if 1 choose hero 1
                 if (heroNumber == 1)
                 {
 
-                    Console.WriteLine("uwu");
-                    Console.ReadLine();
+                    //creates an instance of class
                     Hero_1 Character = new Hero_1();
 
                     Enemy Enemy = new Enemy();
@@ -38,9 +36,10 @@ namespace FightSim
                     Character.name = Console.ReadLine();
 
 
-
+                    // if character is alive
                     while (Character.hp > 0)
                     {
+                        //ask what does player wants to do
                         string actionChoice;
 
                         int action;
@@ -50,20 +49,23 @@ namespace FightSim
 
                         while (!int.TryParse(Console.ReadLine(), out action))
                         {
-
+                            //if attack
                             if (action == 1)
                             {
+                                //takes the damage from attack method and puts it into an int
                                 int damage = Character.Attack(Character.damage);
 
-
+                                // then changes the enemy damage to after the attack
                                 Enemy.hp = Enemy.hp - damage;
                             }
+                            // if defence 
                             else if (action == 2)
                             {
+                                //create an int with damge and take the hurt method's damage into it
                                 int damage = Character.Hurt(Character.damage);
-
+                                //create an int for amount of defence 
                                 int defence = Character.Defence(Character.defence);
-
+                                //subtract the damage after the defence difference into player hp
                                 Character.hp = Character.hp - (damage - defence);
                             }
                             else
